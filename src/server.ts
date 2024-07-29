3181badf2f25fc7f754b2a8c6408b0d1d0f3a16c
Alejandro Aguilar <98495755+TheMayanCitizen@users.noreply.github.com>
2024-07-29 14:37:33 -0700

    Agregue un comentario

--- src/server.ts
+++ src/server.ts
@@ -1,29 +1,29 @@
-import express, {Router} from "express";
+import express, { Router } from "express";
 
 interface Options {
-    port: number;
-    router: Router
+  port: number;
+  router: Router;
 }
 
 export class Server {
-    public readonly app = express();
-    private readonly port: number;
-    private readonly routes: Router;
+  public readonly app = express();
+  private readonly port: number;
+  private readonly routes: Router;
 
-    constructor(options: Options){
-        this.port = options.port;
-        this.routes = options.router;
+  constructor(options: Options) {
+    this.port = options.port;
+    this.routes = options.router;
+  }
+  async start() {
+    this.app.use(express.json());
+    this.app.use(express.urlencoded({ extended: true }));
 
-    }
-    async start(){
-        this.app.use(express.json());
-        this.app.use(express.urlencoded({extended: true}));
+    this.app.use(this.routes);
 
-        this.app.use(this.routes);
+    this.app.listen(this.port, () => {
+      console.log(`Servidor corriendo en puerto ${this.port} 👌`);
+    });
+  }
 
-        this.app.listen(this.port, () => {
-            console.log (`Servidor corriendo en puerto ${this.port} 👌`)
-        })
-    }
-
+  //Cambios hechos por yamil
 }
