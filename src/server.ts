19d514852868f509bdf12d0c20166d42428e5ecb
Jhon Alzate <alzatejf@gmail.com>
2024-07-29 16:32:40 -0500

    Comienza proyecto

---
+++ src/server.ts
@@ -0,0 +1,29 @@
+import express, {Router} from "express";
+
+interface Options {
+    port: number;
+    router: Router
+}
+
+export class Server {
+    public readonly app = express();
+    private readonly port: number;
+    private readonly routes: Router();
+
+    constructor(options: Options){
+        this.port = options.port;
+        this.routes = options.router;
+
+    }
+    async start(){
+        this.app.use(express.json());
+        this.app.use(express.urlencoded({extended: true}));
+
+        this.app.use(this.routes);
+
+        this.app.listen(this.port, () => {
+            console.log (`Servidor corriendo en puerto ${this.port} 👌`)
+        })
+    }
+
+}
