c99cf1822295868860fbbd5b6503adf75269486b
Alejandro Aguilar <98495755+TheMayanCitizen@users.noreply.github.com>
2024-07-31 15:45:38 -0700

    agregada ruta general de productos

--- src/presentation/routes.ts
+++ src/presentation/routes.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import { UsuarioRoutes } from "./usuarios/usuarios.routes";
+import { ProductosRoutes } from "./productos/route";
 
 export class AppRoutes {
   static get routes(): Router {
     const router = Router();
 
     router.use("/api/v1/usuario", UsuarioRoutes.routes);
+    router.use("/api/v1/productos", ProductosRoutes.routes);
 
     return router;
   }
