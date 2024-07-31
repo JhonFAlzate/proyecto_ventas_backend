8ce7717fe5b518eba8a456462e8972238eec714f
Jhon Alzate <alzatejf@gmail.com>
2024-07-31 16:52:19 -0500

    Termino los endpoint de Productos

--- src/presentation/productos/controller.ts
+++ src/presentation/productos/controller.ts
@@ -2,13 +2,15 @@
 
 
 import { Request, Response } from "express";
-import { CustomError } from "../../domain";
+import { CustomError, ProductoDto } from "../../domain";
+import { ProductoService } from "../services/producto.service";
+import { UpdateProductosDto } from "../../domain/dtos/update-producto.dto";
 
 
 export class ProductosController {
 
     constructor(
-        //Dependency injection
+        private readonly productoService: ProductoService
     ){}
 
     private handleError = (error: unknown, res: Response) => {
@@ -17,11 +19,61 @@ export class ProductosController {
         }
 
         console.log(error)
-        return res.status(500).json({message: 'Something went very wrong 🧨🧨🧨'})
+        return res.status(500).json({message: 'Something went very wrong 🧨🧨'})
     }
 
     createProducto = (req: Request, res: Response) => {
-        //implementar el metodo
-        return res.status(200).json({ message: 'Hello World'})
+        const [error, createProductoDto] = ProductoDto.createProductoDto(req.body);
+
+        if(error) return res.status(422).json({message:error});
+
+        this.productoService.createProducto(createProductoDto!)
+        .then((producto) => res.status(201).json(producto))
+        .catch((error: unknown) => {
+
+            return this.handleError(error, res);
+        })
+
+       
     }
+
+    getAllProductos = async (req: Request, res: Response) => {
+        this.productoService.findAllProductos()
+        .then((producto) => res.status(200).json(producto))
+        .catch((error: unknown) => this.handleError(error, res))
+    }
+
+    getOneProducto = async (req: Request, res: Response) => {
+        const {id} = req.params;
+
+        this.productoService.findOneProduct(+id)
+        .then((producto) => res.status(200).json(producto))
+        .catch((error: unknown) => this.handleError(error, res))
+    }
+
+    updateProductos = (req: Request, res: Response) => {
+        const {id} = req.params;
+        const [error, updateProductosDto] = UpdateProductosDto.updateProducto(req.body);
+        if(isNaN(+id)){
+            return res.status(400).json({message: "El id debe ser un número"});
+        }
+        if (error) return res.status(422).json({message: error});
+
+        this.productoService.updateProductos(updateProductosDto!, +id)
+        .then((producto) => res.status(200).json(producto))
+        .catch((error: unknown) => this.handleError(error, res))
+    }
+
+    deleteProductoById = (req: Request, res: Response) => {
+        const {id} = req.params;
+
+        if(isNaN(+id)){
+            return res.status(400).json({message: "El id debe ser un número"})
+        }
+        this.productoService.deleteProducto(+id)
+
+        .then((producto) => res.status(204).json(producto))
+        .catch((error: unknown) => this.handleError(error, res))
+    }
+
 }
--- src/presentation/productos/route.ts
+++ src/presentation/productos/route.ts
@@ -1,6 +1,8 @@
 
 
 import { Router } from "express";
+import { ProductosController } from "./controller";
+import { ProductoService } from "../services/producto.service";
 
 
 
@@ -8,8 +10,16 @@ export class ProductosRoutes {
 
     static get routes(): Router {
         const router = Router();
+        const productoService = new ProductoService();
+        const controller = new ProductosController(productoService);
 
-        return router
+        router.post("/", controller.createProducto);
+        router.get("/", controller.getAllProductos);
+        router.get("/:id", controller.getOneProducto);
+        router.patch("/:id", controller.updateProductos);
+        router.delete("/:id", controller.deleteProductoById);
+
+        return router;
     }
 
 }
